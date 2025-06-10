b9b2dab906d2e46b0bd3012ee3705b38afad6e1b
islanio2 <136517266+islanio2@users.noreply.github.com>
2025-06-10 20:25:56 -0300

    Add files via upload

---
+++ Q3_quickSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
+std::vector<int> vetor_aleatorio(int tamanho, int min, int max) {
+    std::vector<int> vetor;
+    srand(time(0));
+    for (int i = 0; i < tamanho; i++) {
+        vetor.push_back(rand() % (max - min + 1) + min);
+    }
+    return vetor;
+}
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) { 
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    std::vector<int> numeros = vetor_aleatorio(50, 1, 500);
+
+    std::cout << "50 números aleatórios:\n";
+    for (int i = 0; i < numeros.size(); ++i) {
+        std::cout << numeros[i] << (i == numeros.size() - 1 ? "" : ", ");
+    }
+    std::cout << "\n";
+
+    quickSort(numeros, 0, numeros.size() - 1);
+
+    std::cout << "\nLista ordenada com QuickSort:\n";
+    for (int i = 0; i < numeros.size(); ++i) {
+        std::cout << numeros[i] << (i == numeros.size() - 1 ? "" : ", ");
+    }
+    std::cout << "\n";
+
+    return 0;
+}
+
